febebf9e25cb06aad86b901172de38be2d827568
Dallin Sly <krave83@gmail.com>
2024-03-13 00:35:38 -0400

    Initial project setup with configuration files and package.json

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+    },
+    extends: ["eslint:recommended", "airbnb-base", "plugin:prettier/recommended"],
+    parserOptions: {
+      ecmaVersion: 12, // Explicitly set to 2021 or 12 which is essentially the same. This ensures the latest ECMAScript features are supported.
+    },
+    rules: {
+      "no-underscore-dangle": [
+        "error",
+        {
+          allow: ["_id"], // Allowing _id (commonly used in MongoDB)
+        },
+      ],
+      "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+      // Any additional custom rules can be added here
+    },
+  };
+  
