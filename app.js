7ae2c644d1b55d3dfd1d456a75dfca27a66c0e7c
Dallin Sly <krave83@gmail.com>
2024-04-11 22:44:31 -0400

    server updates

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:3001",
   })
 );
 
