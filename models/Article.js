9d7f50e9035012d9444df9510e0fc7b29361c21c
Dallin Sly <krave83@gmail.com>
2024-04-19 14:44:30 -0400

    fix

--- models/Article.js
+++ models/Article.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-  author: { type: string, require: false },
+  author: { type: String, require: false },
   searchKeyword: { type: String, required: true },
   content: { type: String, required: true },
   description: { type: String, required: true },
   publishedAt: { type: String, required: true },
-  title: { type: string, require: false },
+  title: { type: String, require: false },
   source: {
     id: { type: String, required: false },
     name: { type: String, required: true },
